177544b0fe988f544fefff580679c775d5aa62c2
Noah Mercer <noah.mercer@gmail.com>
2025-11-17 15:46:42 -0800

    Fixes bugs in metadata update

--- gears/nifti-info/run.py
+++ gears/nifti-info/run.py
@@ -3,13 +3,16 @@
 
 import nibabel as nib
 import fw_gear
+import shutil
 
 with fw_gear.GearContext() as context:
     nifti = context.config.get_input_path("nifti")
     img = nib.load(nifti)
     shape = img.shape
+    nifti_fname = context.config.get_input_filename("nifti")
+    shutil.move(nifti, f'/flywheel/v0/output/{nifti_fname}')
     context.metadata.update_file_metadata(
-        file_ = nifti,
+        file_ = nifti_fname,
         container_type = 'acquisition',
         info = {
             'Rows': shape[0],
