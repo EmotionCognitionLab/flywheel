b9054ee36632227e23b41120946a26f2ac314bd0
Noah Mercer <noah.mercer@gmail.com>
2025-11-17 18:28:13 -0800

    Switch from .metadata.json to SDK for metadata update instead of overwrite

--- gears/nifti-info/run.py
+++ gears/nifti-info/run.py
@@ -3,20 +3,15 @@
 
 import nibabel as nib
 import fw_gear
-import shutil
 
 with fw_gear.GearContext() as context:
     nifti = context.config.get_input_path("nifti")
     img = nib.load(nifti)
     shape = img.shape
+    acq = context.client.get_acquisition(context.config.destination.get("id"))
     nifti_fname = context.config.get_input_filename("nifti")
-    shutil.move(nifti, f'/flywheel/v0/output/{nifti_fname}')
-    context.metadata.update_file_metadata(
-        file_ = nifti_fname,
-        container_type = 'acquisition',
-        info = {
-            'Rows': shape[0],
-            'Columns': shape[1],
-            'Slices': shape[2] if len(shape) > 2 else 1
-        }
-    )
+    acq.update_file_info(nifti_fname, {
+        "Rows": shape[0],
+        "Columns": shape[1],
+        "Slices": shape[2] if len(shape) > 2 else 1
+    })
