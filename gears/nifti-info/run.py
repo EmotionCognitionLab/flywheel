b16b9417635a5680a201a4fe88aec877de998ecc
Noah Mercer <noah.mercer@gmail.com>
2025-11-19 10:21:04 -0800

    Stores voxel dimensions to metadata

--- gears/nifti-info/run.py
+++ gears/nifti-info/run.py
@@ -8,10 +8,14 @@ with fw_gear.GearContext() as context:
     nifti = context.config.get_input_path("nifti")
     img = nib.load(nifti)
     shape = img.shape
+    pixdim = img.header['pixdim']
     acq = context.client.get_acquisition(context.config.destination.get("id"))
     nifti_fname = context.config.get_input_filename("nifti")
     acq.update_file_info(nifti_fname, {
         "Rows": shape[0],
         "Columns": shape[1],
-        "Slices": shape[2] if len(shape) > 2 else 1
+        "Slices": shape[2] if len(shape) > 2 else 1,
+        "VoxelX": float(pixdim[1]),
+        "VoxelY": float(pixdim[2]),
+        "VoxelZ": float(pixdim[3])
     })
